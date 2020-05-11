63e9b951fcac2f64a5505e57c2beef76f1e568c4
didy400 <didy.and@gmail.com>
2020-05-11 16:54:23 +0200

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,15 @@
+# Load the Rails application.
+require_relative 'application'
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+ActionMailer::Base;smtp_settings = {
+    :user_name => ENV['SENDGRID_LOGIN'],
+    :password => ENV['SENDGRID_PWD'],
+    :domain => 'monsite.fr',
+    :adress => 'smtp.sengrid.net',
+    :port => 587,
+    :autentication => :plain,
+    :enable_starttls_auto => true
+}
